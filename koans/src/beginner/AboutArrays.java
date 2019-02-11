edbb960c3a15b5e69a1aae54ef6333ef918d4a34
Sam Hillebrand <samah@bbd.co.za>
2019-02-11 22:52:11 +0200

    Completed 57/265

--- koans/src/beginner/AboutArrays.java
+++ koans/src/beginner/AboutArrays.java
@@ -27,15 +27,21 @@ public class AboutArrays {
     public void anArraysHashCodeMethodDoesNotConsiderElements() {
         int[] array0 = new int[]{0};
         int[] array1 = new int[]{0};
-        assertEquals(Integer.valueOf(array0.hashCode()).equals(array1.hashCode()), __); // not equal!
+        assertEquals(Integer.valueOf(array0.hashCode()).equals(array1.hashCode()), false); // not equal!
         // TODO: ponder the consequences when arrays are used in Hash Collection implementations.
+
+        // Hashcode - a method provided by java.lang.Object that returns an integer 
+        //            representation of the object memory address. By default, this 
+        //            method returns a random integer that is unique for each instance.
+        //            This integer might change between several executions of the application
+        //            and won't stay the same.
     }
 
     @Koan
     public void arraysHelperClassEqualsMethodConsidersElementsWhenDeterminingEquality() {
         int[] array0 = new int[]{0};
         int[] array1 = new int[]{0};
-        assertEquals(Arrays.equals(array0, array1), __);    // whew - what most people assume
+        assertEquals(Arrays.equals(array0, array1), true);    // whew - what most people assume
         // about equals in regard to arrays! (logical equality)
     }
 
@@ -44,33 +50,35 @@ public class AboutArrays {
         int[] array0 = new int[]{0};
         int[] array1 = new int[]{0};
         // whew - what most people assume about hashCode in regard to arrays!
-        assertEquals(Integer.valueOf(Arrays.hashCode(array0)).equals(Arrays.hashCode(array1)), __);
+        assertEquals(Integer.valueOf(Arrays.hashCode(array0)).equals(Arrays.hashCode(array1)), true);
+
+        //Not the syntax difference!
     }
 
     @Koan
     public void arraysAreMutable() {
         final boolean[] oneBoolean = new boolean[]{false};
         oneBoolean[0] = true;
-        assertEquals(oneBoolean[0], __);
+        assertEquals(oneBoolean[0], true);
     }
 
     @Koan
     public void arraysAreIndexedAtZero() {
         int[] integers = new int[]{1, 2};
-        assertEquals(integers[0], __);
-        assertEquals(integers[1], __);
+        assertEquals(integers[0], 1);
+        assertEquals(integers[1], 2);
     }
 
     @Koan
     public void arrayIndexOutOfBounds() {
         int[] array = new int[]{1};
         @SuppressWarnings("unused")
-        int meh = array[1]; // remember 0 based indexes, 1 is the 2nd element (which doesn't exist)
+        int meh = array[0]; // remember 0 based indexes, 1 is the 2nd element (which doesn't exist)
     }
 
     @Koan
     public void arrayLengthCanBeChecked() {
-        assertEquals(new int[1].length, __);
+        assertEquals(new int[1].length, 1);
     }
 
 }
